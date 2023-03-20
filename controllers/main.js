93c98d9c9e5a46fb51675233697a2b7b68e0969b
Samuelalex16 <samuelalexander2156@gmail.com>
2023-03-20 19:01:11 +0530

    first commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,15 +1,27 @@
 const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
 
 const login = async(req, res) => {
     const { username, password } = req.body
+
+
     if (!username || !password) {
         throw new CustomAPIError('Please provide Username and password', 400)
     }
-    console.log(username, password);
-    res.send(`Fake login`)
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
+
+    res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async(req, res) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startWith('Bearer')) {
+        throw new CustomAPIError('No Token', 400)
+    }
+
     const luckyNumber = Math.floor(Math.random() * 100)
     res.status(200).json({ msg: "Hello Samuel Alexander", secret: `Lucky number:${luckyNumber}` })
 }
